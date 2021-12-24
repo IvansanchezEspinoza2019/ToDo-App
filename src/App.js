0cb06ace2f248c1944c1c41ead904800d5de2b4e
Ivan Sanchez <edgarse1945@gmail.com>
2021-12-24 13:00:25 -0600

    All components structure created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import react from 'react'
+import {TodoCounter} from './TodoCounter';
+import { TodoList } from './TodoList';
+import {TodoItem} from './TodoItem'
+import { TodoAddItem } from './TodoAddItem';
+import { TodoSearch } from './TodoSearch';
+
+const TODOs = [
+  {text: "Primera item", completed: false},
+  {text: "Segundo item", completed: false},
+  {text: "Tercer item", completed: false},
+]
 
 function App() {
   return (
     <react.Fragment>
-      {/*<TodoCounter/>*/}
+      {/*Contador de tareas completadas de la lista de TODOs*/ }
+      {<TodoCounter/>}
+
+
+      {/*Barra de busqueda*/}
+      <TodoSearch/>
+
+      {/*Lista de Todos*/}
+      <TodoList>
+          {TODOs.map(todo => (
+            <TodoItem key={todo.text} text={todo.text}/>
+          ))}
+      </TodoList>
+
+      {/*Agrega una nueva Todo*/}
+      <TodoAddItem/>
+
     </react.Fragment>
   );
 }
---
+++ src/TodoAddItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoAddItem(){
+    return(
+        <button>+</button>
+    )
+}
+
+export {TodoAddItem}
